ae90aecb72f99672c5e5d45d783084fa9135f369
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-08-27 23:41:31 +0200

    played card show and animation of cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,10 +10,35 @@ import { CommonModule } from '@angular/common';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-
   pickCardAnimation = false;
-  
-  takeCard(){
-    this.pickCardAnimation = true;
+  dontShow = true;
+  currentCard: any;
+  game:any =  Game;
+  lenghtOfStack:number = 52;
+  constructor() { }
+  ngOnInit(): void {
+    this.newGame();
+  }
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation && this.game.playedCards.length <= 51) {
+      this.currentCard = this.game?.stack.pop();
+      this.pickCardAnimation = true;
+      this.checkCard();
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+        this.lenghtOfStack = this.game.stack.length;
+      }, 1200);
+    }
+  }
+
+  checkCard(){
+    if(this.lenghtOfStack == 1){
+      this.dontShow = false;
+    }
   }
 }
